4d9354b4bc2bbb89d040c8f88acd3e0b44202883
Mayank Kapoor <mayank.kapoor1203@gmail.com>
2023-07-02 16:54:18 +0530

    first commit

---
+++ src/main/java/com/example/repositories/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.example.repositories;
+
+import com.example.dtos.NameState;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.entities.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+	@Query("select new com.example.dtos.NameState(s.name,a.statename) from  Student s join s.address a")
+	public List<NameState> getNameState();
+}
